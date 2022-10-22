4afeb12c1a91c119364b41a8dc857228a2293641
UGONNA2 <okobougonna2021@gmail.com>
2022-10-22 14:46:22 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers between 00 to 99.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int var;
+
+	for (var = 0; var < 99; var++)
+	{
+		int ii;
+
+		for (ii = var + 1; ii < 100; ii++)
+		{
+			putchar((var / 10) + '0');
+			putchar((var % 10) + '0');
+			putchar(' ');
+			putchar((ii / 10) + '0');
+			putchar((ii % 10) + '0');
+
+			if (var != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				continue;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
